c4e88cdba80fceef4e13cf2d5e2654ee1ba81cf9
victorbose328 <victorbose328@gmail.com>
2025-02-25 05:20:40 -0500

    Boolean Assignemnt

---
+++ Boolean Logic Assginment/Boolean Logic Assginment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Boolean_Logic_Assginment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car Insurance Application");
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? (Yes or No)");
+            string answer = Console.ReadLine().ToLower();
+            bool dui = answer == "yes";
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified for Insurance?");
+            bool qualified = (age > 15) && !dui && (tickets <= 3);
+            Console.WriteLine(qualified);
+        }
+    }
+}
