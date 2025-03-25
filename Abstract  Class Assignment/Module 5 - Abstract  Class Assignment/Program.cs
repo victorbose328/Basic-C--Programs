15245ce49f52e1102c8e67c6738e3c23efae8513
victorbose328 <victorbose328@gmail.com>
2025-03-25 01:54:19 -0400

    assignment

---
+++ Abstract  Class Assignment/Module 5 - Abstract  Class Assignment/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace AbstractClassesAssignment
+{
+    // Abstract class to ensure that it cannot be instantiated directly.
+    // This class will define common properties and behavior for derived classes.
+    public abstract class Person
+    {
+        // Properties to store the first name and last name of the person
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Abstract method SayName() that will be implemented by any class inheriting from Person
+        public abstract void SayName();
+    }
+}
+
+namespace AbstractClassesAssignment
+{
+    // Employee class inherits from the Person class.
+    // This class must implement the SayName() method, as it's abstract in the base class.
+    public class Employee : Person
+    {
+        // Implementing the SayName() method for the Employee class.
+        // This method will display the full name of the employee.
+        public override void SayName()
+        {
+            // Using string interpolation for better readability and performance.
+            Console.WriteLine($"Name: {this.FirstName} {this.LastName}");
+        }
+    }
+}
+
+namespace AbstractClassesAssignment
+{
+    // Main class containing the entry point of the program.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate an Employee object with the first name "Sample" and last name "Student".
+            // This initializes the Employee object using object initializers.
+            Employee sam = new Employee { FirstName = "Sample", LastName = "Student" };
+
+            // Call the SayName() method to display the full name of the employee.
+            sam.SayName();
+
+            // Pause the console to allow the user to see the output.
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Methods and Objects Assignment/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MethodsAndObjectsAssignment
+{
+    public class Person
+    {
+        //Create a class called Person and give it two properties, each of data type string
+        //One called FirstName, the other LastName
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //Give the class a void method called SayName() that takes no parameters and simply writes
+        //the person's full name to the console in the format of: "Name: [full name]"
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + this.FirstName + " " + this.LastName);
+        }
+    }
+}
+
+namespace MethodsAndObjectsAssignment
+{
+    //Create another class called Employee and have it inherit from the Person class
+    public class Employee : Person
+    {
+        //Give the Employee class a property called Id and have it be of data type int
+        public int Id { get; set; }
+    }
+}
+
+namespace MethodsAndObjectsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inside of the Main method, instantiate and initialize an Employee object with
+            //the first name "Sample" and the last name "Student"
+            Employee sam = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            //Call the superclass method SayName() on the Employee object
+            sam.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
